1f203a9e0b97497a226abfaecbeb65b4b627b5c8
Jan <janfred.pfeifer@gmail.com>
2021-05-30 21:03:52 +0200

    Added basic project structure

---
+++ src/main/java/CardValue.java
@@ -0,0 +1,3 @@
+public enum CardValue {
+    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN;
+}
---
+++ src/main/java/Shell.java
@@ -0,0 +1,7 @@
+public class Shell {
+
+    public static void main(String[] args) {
+        String test = "Hello World!";
+        System.out.println(test);
+    }
+}
