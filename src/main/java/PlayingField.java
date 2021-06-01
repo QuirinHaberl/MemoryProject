9b8401e50b880227986e92b804bb4de3114fd8c1
Jan <janfred.pfeifer@gmail.com>
2021-06-02 00:01:32 +0200

    Modified PlayingField.java. Added the methods getCard() and removeCards() and corrected tipos.

--- src/main/java/PlayingField.java
+++ src/main/java/PlayingField.java
@@ -1,18 +1,16 @@
 /**
- * This class PlayingField implements the playing field
+ * The class PlayingField implements the playing field of the memory game
  * as a two-dimensional array.
  * In addition, a controlArray of the boolean type is created to store
  * the value "ist aufgedeckt".
  */
 public class PlayingField {
 
-    // Attributes
-
     /**
      * Hight and Width of the playingfield
      */
-    private final int hight = 4;
-    private final int width = 4;
+    private final int HEIGHT = 4;
+    private final int WIDTH = 4;
 
     /**
      * Array of the playiingfield
@@ -24,27 +22,20 @@ public class PlayingField {
      */
     private boolean[][] controlArray;
 
-
-    //Constructor
-
     /**
-     * Default constructor that creates a new playingfield and the control board
+     * Default constructor that creates a new playingfield and the controlArray
      */
-    public PlayingField(){
-        controlArray = new boolean [hight][width];
-        board = new Card[hight][width];
-
+    public PlayingField() {
+        controlArray = new boolean[HEIGHT][WIDTH];
+        board = new Card[WIDTH][WIDTH];
     }
 
-    //Methods
-
     /**
-    * @return the playingfield
-    */
+     * @return the playingfield
+     */
     public Card[][] getBoard() {
         return board;
     }
-
 
     /**
      * @return the control board
@@ -55,46 +46,57 @@ public class PlayingField {
 
     /**
      * Checks whether a card identified by row and col has already been turned over.
-     * If true, then the image of the card is open, if false, then the image is still hidden.
+     * If true, then the image of the card is open, if false, then the image is closed.
      *
      * @param row Row position of the selected card
      * @param col Column position of the selected card
      * @return whether a selected card is already revealed
      */
-
     public boolean isOpen(int row, int col) {
-        boolean isopen = controlArray[row][col];
-        return isopen;
+        return controlArray[row][col];
     }
 
     /**
      * Checks whether all cards has already been turned over.
      * If true, then the images of all cards are open,
-     * if false, then min. one image is still hidden.
+     * if false, at least one image is still hidden.
      *
      * @return whether all cards are open or not.
      */
-
     public boolean areAllCardsOpen() {
-        boolean allCardsOpen = true;
-        for(int row = 0; row < controlArray.length ; ++row){
+        boolean allCardsOpen = false;
+        for (int row = 0; row < controlArray.length; ++row) {
             for (int col = 0; col < controlArray[row].length; ++col) {
-                if (isOpen(row,col)) {
-                    continue;
-                } else allCardsOpen = false;
+                if (!(isOpen(row, col))) {
+                    return false;
+                }
             }
         }
-        return allCardsOpen;
+        return true;
     }
 
-    /*Noch zu implementieren und kommentieren:
+    /**
+     * Returns a specified card.
+     *
+     * @param row Row position of the selected card
+     * @param col Column position of the selected card
+     * @return the selected card
+     */
+    public Card getCard(int row, int col) {
+        return board[row][col];
+    }
 
-    public Card getCard() {}
 
-    public void removeCards(...){}
-
-
-    */
-
-
+    /**
+     * Removes a pair from the board-array.
+     *
+     * @param row1 Row position of the first selected card
+     * @param col1 Column position of the first selected car
+     * @param row2 Row position of the second selected card
+     * @param col2 Column position of the second selected car
+     */
+    public void removeCards(int row1, int col1, int row2, int col2) {
+        board[row1][col1] = null;
+        board[row2][col2] = null;
+    }
 }
