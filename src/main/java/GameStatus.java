64139850cd1637c3e5ffe37dd1e58ce0d38f14c3
DaqianZhang <zhang37@ads.uni-passau.de>
2021-06-02 08:32:32 +0200

    #

--- src/main/java/GameStatus.java
+++ src/main/java/GameStatus.java
@@ -1,3 +1,8 @@
 public enum GameStatus {
+    /**
+     * enum
+     * this calss created three GameStatus NOTSTART,RUNNING,END.
+     * these are used for controlling if the game can continue.
+     */
     NOTSTART, RUNNING, END;
 }
