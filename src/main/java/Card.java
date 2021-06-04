0096c73af97ec94d050a39affd845ef6be3d6aeb
Jan <janfred.pfeifer@gmail.com>
2021-06-04 23:55:02 +0200

    Revert "cherry-picked the merge into master"
    This reverts commit f6d927918015f251e52d87d470e2bfeb93992ebc.

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Card {
     CardValue value;
 
@@ -11,36 +10,3 @@ public class Card {
         return value;
     }
 }
-=======
-/**
- * The class {@link Card} implements a card of the game.
- */
-public class Card {
-
-    /**
-     * The attribute {@link CardValue} is the image of the card.
-     */
-    CardValue value;
-
-    /**
-     * The attribute {@code revealed} is the image of the card.
-     */
-    boolean revealed;
-
-    /**
-     * Constructs a new {@link Card}.
-     *
-     * @param value uses the enumerations of {@link CardValue}
-     */
-    public Card(CardValue value) {
-        this.value = value;
-    }
-
-    /**
-     * @return the value of a {@link Card}.
-     */
-    public CardValue getValue() {
-        return value;
-    }
-}
->>>>>>> cfe11c1 (Merge pull request #73 from se2p-se/pull_request_Isabella)
--- src/main/java/RevealedCard.java
+++ src/main/java/RevealedCard.java
@@ -1,19 +1,12 @@
 /**
-<<<<<<< HEAD
  * <heading>RevealedCard</heading>
  * This class is a helper class to transfer information about a revealed card
-=======
- * This class is a helper class to transfer information about a {@link RevealedCard}
->>>>>>> cfe11c1 (Merge pull request #73 from se2p-se/pull_request_Isabella)
  * form Control to Visualisation.
  */
 public class RevealedCard {
 
-<<<<<<< HEAD
     //Attributes
 
-=======
->>>>>>> cfe11c1 (Merge pull request #73 from se2p-se/pull_request_Isabella)
     /**
      * Indicator whether the card can be revealed
      */
@@ -29,12 +22,9 @@ public class RevealedCard {
      */
     private Card revealedCard;
 
-<<<<<<< HEAD
 
     //Methods
 
-=======
->>>>>>> cfe11c1 (Merge pull request #73 from se2p-se/pull_request_Isabella)
     /**
      * Getter of the indicator
      *
@@ -88,8 +78,4 @@ public class RevealedCard {
     public void setRevealedCard(Card revealedCard) {
         this.revealedCard = revealedCard;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> cfe11c1 (Merge pull request #73 from se2p-se/pull_request_Isabella)
