eb383848714d465cc47c213633eed0e8c1fc22ec
Jan <janfred.pfeifer@gmail.com>
2021-06-04 23:51:37 +0200

    Revert "cherry-picked the merge into master"
    This reverts commit f6d927918015f251e52d87d470e2bfeb93992ebc.

--- src/main/java/CardValue.java
+++ src/main/java/CardValue.java
@@ -1,21 +1,11 @@
 /**
-<<<<<<< HEAD
  * cardValue that can be used.
  */
 
-=======
- * Enumeration of values that can be used.
- */
->>>>>>> cfe11c1 (Merge pull request #73 from se2p-se/pull_request_Isabella)
 public enum CardValue {
     /**
      * Numbers from ONE to EIGHT
      */
-<<<<<<< HEAD
     //16 Karte, 8 Paare, insgesamt 8 Werte
     ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT;
 }
-=======
-    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
-}
->>>>>>> cfe11c1 (Merge pull request #73 from se2p-se/pull_request_Isabella)
