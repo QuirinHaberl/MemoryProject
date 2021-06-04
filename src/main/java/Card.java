db6a94a9983c5284e76ac137aad21eaae4a14d62
Jan <janfred.pfeifer@gmail.com>
2021-06-04 14:13:46 +0200

    Modified comments and documentation.

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,28 +1,30 @@
+/**
+ * The class Card implements the card of the memory game.
+ */
 public class Card {
-    /**
-     * The class Card implements the card of the memory game.
-     */
 
     //Attributes
     /**
-     * This value is the image of the card
+     * This {@link CardValue} is the image of the card
      */
     CardValue value;
 
 
     //Constructor
     /**
-     * Default constructor that creates a new card.
+     * Constructs a new {@link Card}. Initially, the value of a given cart is null.
+     *
+     * @param value enum value of {@link CardValue}
      */
-    public Card(CardValue value){
+    public Card(CardValue value) {
         this.value = value;
     }
 
     //Methods
     /**
-     * @return the card value.
+     * @return the card value of {@link CardValue}.
      */
-    public CardValue getValue(){
+    public CardValue getValue() {
 
         return value;
     }
--- src/main/java/CardValue.java
+++ src/main/java/CardValue.java
@@ -1,11 +1,10 @@
 /**
- * cardValue that can be used.
+ * Values that can be used.
  */
 
 public enum CardValue {
     /**
      * Numbers from ONE to EIGHT
      */
-    //16 Karte, 8 Paare, insgesamt 8 Werte
-    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT;
+    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
 }
