9e8ba23ea735932d3b121af3daa7e4f8b4c96e1d
Jan <janfred.pfeifer@gmail.com>
2021-06-06 17:40:48 +0200

    removed old class-files

--- src/main/java/CardValue.java
+++
@@ -1,9 +0,0 @@
-/**
- * Enumeration of values that can be used.
- */
-public enum CardValue {
-    /**
-     * Numbers from ONE to EIGHT
-     */
-    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
-}
