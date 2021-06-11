5fa5889a1ce5947cb12e1883c3cd3894ef59790b
FlorianMuhrer <flo.muhrer@hotmail.com>
2021-06-11 08:51:22 +0200

    Neues Enum CardLetter erstellt als neues Kartenset

---
+++ src/main/java/Model/CardLetter.java
@@ -0,0 +1,11 @@
+package Model;
+/**
+ * Enumeration of values that can be used.
+ * This is the Cardset with Letters
+ */
+public enum CardLetter {
+    /**
+     * a {@link Card} can have values from A to P
+     */
+    A, B, C, D, F, G, H, I, J, K, L, M, N, O, P
+}
