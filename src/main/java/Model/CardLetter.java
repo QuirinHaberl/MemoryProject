ed1c8fe27b99712bb4bb46ed1159b01d564d9e18
Jan <janfred.pfeifer@gmail.com>
2021-06-12 09:04:01 +0200

    added attibutes to our enums CardLetter and CardValue, added documentation

--- src/main/java/Model/CardLetter.java
+++ src/main/java/Model/CardLetter.java
@@ -1,4 +1,5 @@
 package Model;
+
 /**
  * Enumeration of values that can be used.
  * This is the Cardset with Letters
@@ -7,5 +8,28 @@ public enum CardLetter {
     /**
      * a {@link Card} can have values from A to P
      */
-    A, B, C, D, F, G, H, I, J, K, L, M, N, O, P
+    A("A"), B("B"), C("C"), D("D"), F("F"),
+    G("G"), H("H"), I("I"), J("J"), K("K"),
+    L("L"), M("M"), N("N"), O("O"), P("P");
+
+    /**
+     * The value of a card is saved {@code label}, which can't be changed
+     */
+    private final String letter;
+
+    /**
+     * Constructs a new label
+     *
+     * @param letter is saved as a attribute for a {@link CardLetter}
+     */
+    CardLetter(String letter) {
+        this.letter = letter;
+    }
+
+    /**
+     * @return the {@code label} of a {@link CardLetter}
+     */
+    public String getLetter() {
+        return letter;
+    }
 }
