d26ead5e96917ad3e336aecc59eaf0f73d075c2b
FlorianMuhrer <flo.muhrer@hotmail.com>
2021-06-30 16:04:53 +0200

    deleted redundant Stylesheet

--- src/main/java/de/uni_passau/se/memory/gui/GUI.java
+++ src/main/java/de/uni_passau/se/memory/gui/GUI.java
@@ -27,7 +27,6 @@ public class GUI extends Application {
         Scene scene = new Scene(root);
         stage.setTitle("MEMORY");
         stage.setScene(scene);
-        scene.getStylesheets().add("@stylesheet/Style.css");
         stage.show();
     }
 
