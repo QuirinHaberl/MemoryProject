5ec866ed8a53088a7124298745bffe6e6af8c973
Jan <janfred.pfeifer@gmail.com>
2021-07-05 22:40:21 +0200

    implemented the usage of playerProfiles for the GUI

--- src/main/java/de/uni_passau/se/memory/gui/ControllerPack/GameController.java
+++ src/main/java/de/uni_passau/se/memory/gui/ControllerPack/GameController.java
@@ -6,18 +6,23 @@ import de.uni_passau.se.memory.gui.Window;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.ImageCursor;
 import javafx.scene.Node;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ResourceBundle;
 
 
-public class GameController {
+public class GameController implements Initializable {
 
     public GameController() {
     }
@@ -25,7 +30,6 @@ public class GameController {
     /**
      * Filling the Stage with Objects
      */
-
     public Node newButton(String id, int row, int col) {
         Button button = new Button();
         button.setPrefSize(100, 70);
@@ -40,7 +44,7 @@ public class GameController {
             @Override
             public void handle(ActionEvent event) {
                 button.getStyleClass().removeAll("Card");
-                Object card = DataDisplay.getInstance().getGame().getCard(row,col).getValue();
+                Object card = DataDisplay.getInstance().getGame().getCard(row, col).getValue();
                 ((CardLetters) card).getPicture();
                 button.getStyleClass().add(((CardLetters) card).getPicture());
                 //button.setVisible(false);
@@ -51,21 +55,33 @@ public class GameController {
         return button;
     }
 
+    @FXML
     public Button start;
 
+    @FXML
+    private Label labelPlayer1;
+
+    @FXML
+    private Label labelPlayer2;
+
+    @FXML
+    private Label labelPlayer3;
+
+    @FXML
+    private Label labelPlayer4;
 
     @FXML
     private GridPane Board;
 
     public void startClicked() {
-
+        setPlayerLabel();
         int size = DataDisplay.getInstance().getGame().getPlayingField().getSize();
         DataDisplay.getInstance().getGame().getPlayingField().fillLetters();
 
         for (int row = 0; row < size; row++) {
             Board.getColumnConstraints().add(new ColumnConstraints(100));
             for (int col = 0; col < size; col++) {
-                Board.add(newButton("(" + row + " " + col + ")",row,col),col,row);
+                Board.add(newButton("(" + row + " " + col + ")", row, col), col, row);
             }
         }
     }
@@ -74,7 +90,7 @@ public class GameController {
      * The other actions
      */
     public void menu(ActionEvent actionEvent) {
-        ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).close();
+        ((Stage) (((Button) actionEvent.getSource()).getScene().getWindow())).close();
         new Window("StartScreen.fxml");
     }
 
@@ -82,4 +98,19 @@ public class GameController {
         new Window("Rules.fxml");
     }
 
+    public void setPlayerLabel() {
+        Label[] playerLabels = {
+                labelPlayer1, labelPlayer2, labelPlayer3, labelPlayer4
+        };
+        for (int i = 0; i < DataDisplay.getInstance().getGame().
+                getPlayerAmount(); i++) {
+            playerLabels[i].setText(DataDisplay.getInstance().getGame().
+                    getPlayerList().getPlayer(i).getName());
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        startClicked();
+    }
 }
--- src/main/java/de/uni_passau/se/memory/gui/ControllerPack/NameSelectionController.java
+++ src/main/java/de/uni_passau/se/memory/gui/ControllerPack/NameSelectionController.java
@@ -1,28 +1,80 @@
 package de.uni_passau.se.memory.gui.ControllerPack;
 
+import de.uni_passau.se.memory.Model.Game;
 import de.uni_passau.se.memory.View.DataDisplay;
 import de.uni_passau.se.memory.gui.Window;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class NameSelectionController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NameSelectionController implements Initializable {
+
+    @FXML
+    private TextField player1Input;
+
+    @FXML
+    private Label nameLabel1;
+
+    @FXML
+    private TextField player2Input;
+
+    @FXML
+    private Label nameLabel2;
+
+    @FXML
+    private TextField player3Input;
+
+    @FXML
+    private Label nameLabel3;
+
+    @FXML
+    private TextField player4Input;
+
+    @FXML
+    private Label nameLabel4;
 
     @FXML
     private TextField input = new TextField();
 
-    public void usePlayerProfile(ActionEvent actionEvent) {
+    public void ok(ActionEvent actionEvent) {
+
+        addPlayers();
+
+        ((Stage) (((Button) actionEvent.getSource()).getScene().getWindow())).close();
+        new Window("Levels.fxml");
     }
 
-    public void ok(ActionEvent actionEvent) {
-        if(new String[]{input.getText()}[0] == null) {
-            DataDisplay.getInstance().getGame().addPlayers(1, new String[]{"Spieler 1"});
-        } else {
-            DataDisplay.getInstance().getGame().addPlayers(1, new String[]{input.getText()});
+    public void addPlayers() {
+        Game game = DataDisplay.getInstance().getGame();
+        TextField[] textFields = {player1Input, player2Input, player3Input, player4Input};
+        int playerAmount = game.getPlayerAmount();
+        for (int i = 0; i < playerAmount; i++) {
+            game.addPlayer(textFields[i].getText());
         }
-        ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).close();
-        new Window("Levels.fxml");
+        game.getDatabase().loadPlayerProfiles();
+        game.useProfile(game.getDatabase().getPlayerProfiles());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Label[] labels = {nameLabel1, nameLabel2, nameLabel3, nameLabel4};
+        TextField[] textFields = {player1Input, player2Input, player3Input, player4Input};
+
+        //TODO Man könnte die Labels default auf invisible setzen
+        for (int i = 0; i < labels.length; i++) {
+            labels[i].setVisible(false);
+            textFields[i].setVisible(false);
+        }
+        for (int i = 0; i < DataDisplay.getInstance().getGame().getPlayerAmount(); i++) {
+            labels[i].setVisible(true);
+            textFields[i].setVisible(true);
+        }
     }
 }
