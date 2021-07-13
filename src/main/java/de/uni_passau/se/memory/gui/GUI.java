2a1c99d8958369f79e9525c636c82f31e2b69626
FlorianMuhrer <flo.muhrer@hotmail.com>
2021-07-13 08:24:20 +0200

    Javadocs and README

--- src/main/java/de/uni_passau/se/memory/gui/GUI.java
+++ src/main/java/de/uni_passau/se/memory/gui/GUI.java
@@ -1,12 +1,10 @@
 package de.uni_passau.se.memory.gui;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -18,8 +16,6 @@ import java.util.Objects;
  * The GUI of this program.
  */
 public class GUI extends Application {
-    private double xOffSet = 0;
-    private double yOffSet = 0;
     /**
      * The Main-methode of the GUI.
      *
@@ -29,6 +25,15 @@ public class GUI extends Application {
 
         launch();
     }
+
+    /**
+     * Stores the x-Position of the Screen
+     */
+    private double xOffSet = 0;
+    /**
+     * Stores the y-Position of the Screen
+     */
+    private double yOffSet = 0;
 
     /**
      * Starts the gui.
@@ -45,19 +50,14 @@ public class GUI extends Application {
         stage.setTitle("MEMORY");
         stage.initStyle(StageStyle.UNDECORATED);
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("Images/Card.png"))));
-        root.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffSet = event.getSceneX();
-                yOffSet = event.getSceneY();
-            }
+        //Makes Stage draggable
+        root.setOnMousePressed(event -> {
+            xOffSet = event.getSceneX();
+            yOffSet = event.getSceneY();
         });
-        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                stage.setX(event.getScreenX()-xOffSet);
-                stage.setY(event.getScreenY()-yOffSet);
-            }
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX()-xOffSet);
+            stage.setY(event.getScreenY()-yOffSet);
         });
         stage.setScene(scene);
         stage.show();
